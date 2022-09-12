1d439321059bb95051f2c183cf6cd87bf90c8254
mahimagupta24 <mahimagupta2401@gmail.com>
2022-09-12 19:33:37 +0530

    docs added

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
+
 checkButton.addEventListener("click", validateBillAndCashAmount);
 
-
 function validateBillAndCashAmount() {
-  hideMessage()
-  var cash=Number(cashGiven.value);
-var bill=Number(billAmount.value);
+  hideMessage();
+  var cash = Number(cashGiven.value);
+  var bill = Number(billAmount.value);
 
-  if((bill>0) && (cash>0)){
-    if(cash<bill){
+  if (bill > 0 && cash > 0) {
+    if (cash < bill) {
       showMessage("Do you wanna wash plates?", "orange");
-     }else if(bill==cash){
-      showMessage("Thanks for paying the cash exactly equals to bill amount.","blue")
-     }
-     else{
-      const amountToBeReturned = (cash-bill);
-    calculateChange(amountToBeReturned);
-  } 
-  
-} else {
+    } else if (bill == cash) {
+      showMessage(
+        "Thanks for paying the cash exactly equals to bill amount.",
+        "blue"
+      );
+    } else {
+      const amountToBeReturned = cash - bill;
+      calculateChange(amountToBeReturned);
+    }
+  } else {
     showMessage("Invalid bill amount", "red");
   }
 }
-
 
 function calculateChange(amountToBeReturned) {
   for (let i = 0; i < availableNotes.length; i++) {
@@ -48,5 +48,3 @@ function showMessage(msg, color) {
   message.innerText = msg;
   message.style.color = color;
 }
-
-
